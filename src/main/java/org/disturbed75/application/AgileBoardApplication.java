9cc6952e22315a858cd780dbc9f3e2f0cc973b07
andrew-okhrimchuk <andrew456gold@gmail.com>
2019-02-17 20:12:07 +0200

    clearned some links

--- src/main/java/org/disturbed75/application/AgileBoardApplication.java
+++ src/main/java/org/disturbed75/application/AgileBoardApplication.java
@@ -1,9 +1,6 @@
 package org.disturbed75.application;
 
 
-import org.disturbed75.application.DAO.UserDAO;
-import org.disturbed75.application.container.ValuesContainer;
-import org.disturbed75.application.entity.Column;
 import org.disturbed75.application.entity.User;
 import org.disturbed75.application.service.ColumnService;
 import org.disturbed75.application.service.MyUserDetailsService;
@@ -12,9 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import java.util.ArrayList;
 
 @SpringBootApplication(scanBasePackages ={"org.disturbed75.application.service","org.disturbed75.application.controller", "org.disturbed75.application.security"})
 @EnableAutoConfiguration
@@ -41,24 +36,6 @@ public class AgileBoardApplication implements CommandLineRunner {
 			user.setPassword("admin");
 			myUserDetailsService.save(user);
 		}
-
-		/*if(columnService.getColumnByName(ValuesContainer.TO_DO_COLUMN_NAME) == null ||
-				columnService.getColumnByName(ValuesContainer.IN_PROGRESS_COLUMN_NAME) == null ||
-				columnService.getColumnByName(ValuesContainer.DONE_COLUMN_NAME) == null)
-		{
-			new Column(ValuesContainer.TO_DO_COLUMN_NAME);
-			new Column(ValuesContainer.IN_PROGRESS_COLUMN_NAME);
-			new Column(ValuesContainer.DONE_COLUMN_NAME);
-
-
-
-
-
-		}*/
-
-
-
-
 
 	}
 }
