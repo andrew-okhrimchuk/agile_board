04b64b031388b5147ac96c6d852bc2772f97c1ca
andrew-okhrimchuk <andrew456gold@gmail.com>
2019-02-21 07:58:07 +0200

    create a blank for tests.

---
+++ src/test/java/org/disturbed75/application/service/ColumnServiceTest.java
@@ -0,0 +1,20 @@
+package org.disturbed75.application.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ColumnServiceTest {
+    @Test
+    public void addNewColumn() throws Exception {
+    }
+
+    @Test
+    public void getColumnByName() throws Exception {
+    }
+
+    @Test
+    public void getAllColumns() throws Exception {
+    }
+
+}
